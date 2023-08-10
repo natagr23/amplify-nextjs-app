f2890d6d4be989cfd90491c5e5fcc63ce75c9e75
natagr23 <nata_garcia23@hotmail.com>
2023-08-10 14:21:56 -0500

    update dependencies & fix env variables in nextjs

--- src/app/dashboard/(auth)/login/page.js
+++ src/app/dashboard/(auth)/login/page.js
@@ -8,9 +8,10 @@ import '@aws-amplify/ui-react/styles.css';
 // //import awsExports from '../../../../aws-exports';
 
 Amplify.configure({
-  aws_cognito_region: process.env.AWS_COGNITO_REGION, // (required) - Region where Amazon Cognito project was created
-  aws_user_pools_id: process.env.AWS_COGNITO_USER_POOL_ID, // (optional) -  Amazon Cognito User Pool ID
-  aws_user_pools_web_client_id: process.env.AWS_COGNITO_USER_POOL_APP_CLIENT_ID, // (optional) - Amazon Cognito App Client ID (App client secret needs to be disabled)
+  aws_cognito_region: process.env.NEXT_PUBLIC_AWS_COGNITO_REGION, // (required) - Region where Amazon Cognito project was created
+  aws_user_pools_id: process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_ID, // (optional) -  Amazon Cognito User Pool ID
+  aws_user_pools_web_client_id:
+    process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_APP_CLIENT_ID, // (optional) - Amazon Cognito App Client ID (App client secret needs to be disabled)
 });
 
 export default function App() {
